a85913206aaa368ab128c15cd1024cd45f492e5c
JAKlingler <120359180+JAKlingler@users.noreply.github.com>
2026-08-20 13:50:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ st.set_page_config(
 USE_SUPABASE = False
 try:
     from supabase import create_client
-    if "SUPABASE_URL" in st.secrets and "SUPABASE_SERVICE_KEY" in st.secrets:
-        sb = create_client(st.secrets["SUPABASE_URL"], st.secrets["SUPABASE_SERVICE_KEY"])
+    if "SUPABASE_URL" in st.secrets and "SUPABASE_KEY" in st.secrets:
+        sb = create_client(st.secrets["SUPABASE_URL"], st.secrets["SUPABASE_KEY"])
         USE_SUPABASE = True
 except Exception:
     sb = None
